6ff5449286aedc9b1e1433d43dc551fb9ae609c0
Knifedwarf <viktor.myhrberg@hotmail.se>
2015-10-15 23:58:07 +0200

    Heavy programming of character creation completed!
    TRY IT OUT!

--- A World of Violence/Assets/CharacterPartButton.cs
+++ A World of Violence/Assets/CharacterPartButton.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class CharacterPartButton : MonoBehaviour {
 
@@ -7,7 +9,40 @@ public class CharacterPartButton : MonoBehaviour {
 
     public Sprite NewSprite;
 
+    public Sprite f_sprite;
+    public Sprite m_sprite;
+
+    public Sprite m_p_sprite;
+    public Sprite m_a_sprite;
+    public Sprite m_f_sprite;
+    public Sprite m_m_sprite;
+
+    public Sprite f_p_sprite;
+    public Sprite f_a_sprite;
+    public Sprite f_f_sprite;
+    public Sprite f_m_sprite;
+
+    public Sprite p_f_p_sprite;
+    public Sprite p_f_a_sprite;
+    public Sprite p_f_f_sprite;
+    public Sprite p_f_m_sprite;
+
+    public int bodyType; // Powerful, Athletic, Fair, Mighty
+    public int skinColor; // Pale, Bright, Tanned, Dark
+
     CharacterProperties TheCharacter;
+
+    public GameObject NextOptionState;
+    public GameObject PreviousOptionState;
+
+    public bool female;
+
+    public bool resetSprites;
+
+    public bool canContinue;
+
+    public bool isChest;
+    public bool pushedBoobs;
 
 	void Start () 
     {
@@ -15,11 +50,190 @@ public class CharacterPartButton : MonoBehaviour {
 	}
 
 
-    void ChangeSprite()
+    public void ChangeSprite()
     {
-        CharacterPart.sprite = NewSprite;
+        bool condition = false;
+        if (TheCharacter.female)
+        {
+            if (skinColor != 0)
+            {
+                
+                if(TheCharacter.Body.sprite == TheCharacter.pushedBoobs)
+                {
+                    condition = true;
+                }
+                TheCharacter.unpushedBoobs = NewSprite;
+                if (TheCharacter.bodyType == 1)
+                {
+                    TheCharacter.pushedBoobs = p_f_p_sprite;
+                }
+                else if (TheCharacter.bodyType == 2)
+                {
+                    TheCharacter.pushedBoobs = p_f_a_sprite;
+                }
+                else if (TheCharacter.bodyType == 3)
+                {
+                    TheCharacter.pushedBoobs = p_f_f_sprite;
+                }
+                else if (TheCharacter.bodyType == 4)
+                {
+                    TheCharacter.pushedBoobs = p_f_m_sprite;
+                }
+            }
+            if (isChest)
+            {
+
+                TheCharacter.SetPushedBoobs(pushedBoobs);
+
+            }
+            if(condition)
+            {
+                CharacterPart.sprite = TheCharacter.pushedBoobs;
+            }
+            else
+            {
+                CharacterPart.sprite = NewSprite;
+            }
+        }
+        else CharacterPart.sprite = NewSprite;
+        TheCharacter.readyToProceed = true;
+        
+        TheCharacter.pendingBodyType = bodyType;
+        TheCharacter.pendingSkinColor = skinColor;
+        
     }
 
+    public void SetSex()
+    {
+        TheCharacter.readyToProceed = true;
+        TheCharacter.female = female;
+    }
 
+    public void SetBodyType()
+    {
+        //TheCharacter.readyToProceed = true;
+        TheCharacter.bodyType = TheCharacter.pendingBodyType;
+        
+    }
 
+    public void SetSkinColor()
+    {
+        if (TheCharacter.skinColor == 0)
+        {
+            TheCharacter.skinColor = 2;
+            TheCharacter.readyToProceed = true;
+            if (TheCharacter.bodyType == 1)
+            {
+                TheCharacter.unpushedBoobs = f_p_sprite;
+                TheCharacter.pushedBoobs = p_f_p_sprite;
+            }
+            else if (TheCharacter.bodyType == 2)
+            {
+                TheCharacter.unpushedBoobs = f_a_sprite;
+                TheCharacter.pushedBoobs = p_f_a_sprite;
+            }
+            else if (TheCharacter.bodyType == 3)
+            {
+                TheCharacter.unpushedBoobs = f_f_sprite;
+                TheCharacter.pushedBoobs = p_f_f_sprite;
+            }
+            else if (TheCharacter.bodyType == 4)
+            {
+                TheCharacter.unpushedBoobs = f_m_sprite;
+                TheCharacter.pushedBoobs = p_f_m_sprite;
+            }
+
+        }
+        TheCharacter.skinColor = TheCharacter.pendingSkinColor;
+    }
+
+    public void PreviousOption()
+    {
+        PreviousOptionState.SetActive(true);
+        transform.parent.gameObject.SetActive(false);
+        
+    }
+
+    public void DisableChildren()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+
+    }
+
+    public void ResetCharacter()
+    {
+        TheCharacter.ResetProperties(resetSprites);
+    }
+
+    public void Next()
+    {
+        if (TheCharacter.readyToProceed)
+        {
+            TheCharacter.readyToProceed = false;
+            NextOptionState.SetActive(true);
+            transform.parent.gameObject.SetActive(false);
+        }
+    }
+
+    void OnEnable()
+    {
+        TheCharacter = GameObject.Find("Character").GetComponent<CharacterProperties>();
+        if(canContinue)
+        {
+            TheCharacter.readyToProceed = true;
+        }
+        else TheCharacter.readyToProceed = false;
+
+        if(TheCharacter.female)
+        {
+            if (TheCharacter.bodyType == 0)
+            {
+                NewSprite = f_sprite;
+                TheCharacter.pendingBodyType = bodyType;
+            }
+            else if(TheCharacter.bodyType == 1)
+            {
+                NewSprite = f_p_sprite;
+            }
+            else if (TheCharacter.bodyType == 2)
+            {
+                NewSprite = f_a_sprite;
+            }
+            else if (TheCharacter.bodyType == 3)
+            {
+                NewSprite = f_f_sprite;
+            }
+            else if (TheCharacter.bodyType == 4)
+            {
+                NewSprite = f_m_sprite;
+            }
+        }
+        else 
+        {
+            if (TheCharacter.bodyType == 0)
+            {
+                NewSprite = m_sprite;
+                TheCharacter.pendingBodyType = bodyType;
+            }
+            else if (TheCharacter.bodyType == 1)
+            {
+                NewSprite = m_p_sprite;
+            }
+            else if (TheCharacter.bodyType == 2)
+            {
+                NewSprite = m_a_sprite;
+            }
+            else if (TheCharacter.bodyType == 3)
+            {
+                NewSprite = m_f_sprite;
+            }
+            else if (TheCharacter.bodyType == 4)
+            {
+                NewSprite = m_m_sprite;
+            }
+        }
+    }
 }
--- A World of Violence/Assets/CharacterProperties.cs
+++ A World of Violence/Assets/CharacterProperties.cs
@@ -3,13 +3,48 @@ using System.Collections;
 
 public class CharacterProperties : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public bool female;
+    public int bodyType; // Powerful, Athletic, Fair, Mighty
+    public int pendingBodyType;
+    public int skinColor;
+    public int pendingSkinColor;
+    public bool readyToProceed=false;
+
+    public Sprite pushedBoobs;
+    public Sprite unpushedBoobs;
+    public SpriteRenderer Body;
+	void Start () 
+    {
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
+
+    public void ResetProperties(bool resetSprites)
+    {
+        if(resetSprites)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).GetComponent<SpriteRenderer>().sprite = null;
+            }
+        }
+        bodyType = 0;
+        skinColor = 0;
+    }
+
+    public void SetPushedBoobs(bool ispushed)
+    {
+        if(ispushed)
+        {
+            Body.sprite = pushedBoobs;
+        }
+        else
+        {
+            Body.sprite = unpushedBoobs;
+        }
+    }
 }
