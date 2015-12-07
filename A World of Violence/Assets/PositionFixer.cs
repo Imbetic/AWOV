4970ae28ec6351df0200021a02e71100d45b0730
Knifedwarf <viktor.myhrberg@hotmail.se>
2015-12-07 03:38:32 +0100

    FUCKING FUCK FUCK FUCK UINITY PIECE OF FUCKING SHIT NEVER AGAIN om inte bara för nåt dåligt jävla cp-småspel det är fan allt den här skiten
    duger till

---
+++ A World of Violence/Assets/PositionFixer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionFixer : MonoBehaviour {
+
+    public RectTransform FixPos;
+
+	// Use this for initialization
+	void Start () 
+    {
+        GetComponent<RectTransform>().position= FixPos.position;
+        Debug.Log(GetComponent<RectTransform>().position + " " + FixPos.position);
+	}
+	 void OnEnable()
+    {
+        GetComponent<RectTransform>().position = FixPos.position;
+    }
+	// Update is called once per frame
+	void Update () {
+        GetComponent<RectTransform>().position = FixPos.position;
+	}
+}
